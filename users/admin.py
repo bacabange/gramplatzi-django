d522f4246a1ff7fa16d84fe4a6bf5ee69dc641d2
Stiven Castillo <stivencastillo.90@gmail.com>
2020-08-23 10:43:23 -0500

    Create post app, edit admin users

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from users.models import Profile
 
 # Register your models here.
@@ -9,3 +11,42 @@ class ProfileAdmin(admin.ModelAdmin):
     list_editable = ('phone_number', 'website')
     search_fields = ('user__email', 'user__first_name', 'user__last_name', 'phone_number')
     list_filter = ('created', 'modified')
+    fieldsets = (
+        ('Profile', {
+            'fields': (
+                ('user', 'picture'),
+            ),
+        }),
+        ('Extra Info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography'),
+            ),
+        }),
+        ('Metadata', {
+            'fields': (
+                ('created', 'modified'),
+            ),
+        }),
+    )
+
+    readonly_fields = ('created', 'modified', 'user')
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
