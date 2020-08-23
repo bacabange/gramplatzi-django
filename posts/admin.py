8d5f65f21c2e84b37b97a5101b7ed10fc06a9165
Stiven Castillo <stivencastillo.90@gmail.com>
2020-08-23 11:03:23 -0500

    Configure Post to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,31 @@
+# Django
 from django.contrib import admin
+# Models
+from posts.models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'profile', 'title', 'photo')
+    list_display_links = ('pk', 'user')
+
+    search_fields = ('user__username', 'title',)
+    list_filter = (
+        'created',
+        'modified',
+        'user__is_active',
+        'user__is_staff'
+    )
+    readonly_fields = ('created', 'modified', 'user')
+
+    fieldsets = (
+        ('Post', {
+            'fields': (('title', 'photo'),)
+        }),
+
+        ('Metadata', {
+            'fields': (
+                ('user',),
+                ('created', 'modified'),
+            )
+        })
+    )
