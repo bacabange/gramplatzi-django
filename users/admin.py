3d7fadbc71376d3fc1e527b7c434b4630758e6f6
Stiven Castillo <stivencastillo.90@gmail.com>
2020-08-23 10:04:24 -0500

    Create user app

---
+++ users/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from users.models import Profile
+
+# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number', 'website')
+    search_fields = ('user__email', 'user__first_name', 'user__last_name', 'phone_number')
+    list_filter = ('created', 'modified')
